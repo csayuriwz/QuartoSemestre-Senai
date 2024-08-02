41b4e98912af64478d3e5e0eadcf9dadf3b9bad4
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2024-08-02 14:31:44 -0300

    testes

---
+++ AtividadesTestes/email/Emails.Test/ValidateEmail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using validate_email;
+
+namespace Emails.Test
+{
+    public class ValidateEmail
+    {
+        [Theory]
+        [InlineData("exemplo@dominio.com", true)]
+        [InlineData("exemplo.dominio.com", false)] // Não contém "@"
+        [InlineData("@dominio.com", false)] // Não contém "."
+        [InlineData("exemplo@", false)] // Não contém "."
+        [InlineData("", false)] // String vazia
+        public void ValidateEmailTest(string email, bool expected)
+        {
+            var validator = new Email();
+  
+            bool result = validator.EmailValidate(email);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ AtividadesTestes/email/validate-email/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace validate_email
+{
+    public class Email
+    {
+        public bool EmailValidate(string email)
+        {
+            // Verifica se o endereço de e-mail contém "@" e "."
+            return email.Contains("@") && email.Contains(".");
+        }
+    }
+}
