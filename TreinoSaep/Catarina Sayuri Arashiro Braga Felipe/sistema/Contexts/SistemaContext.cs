3893927100fbc4860095fa8295135328c56cdea4
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2024-10-11 17:18:01 -0300

    treino saep

---
+++ TreinoSaep/Catarina Sayuri Arashiro Braga Felipe/sistema/Contexts/SistemaContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using sistema.Models;
+
+namespace sistema.Contexts;
+
+public partial class SistemaContext : DbContext
+{
+    public SistemaContext()
+    {
+    }
+
+    public SistemaContext(DbContextOptions<SistemaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Atividade> Atividades { get; set; }
+
+    public virtual DbSet<Professor> Professors { get; set; }
+
+    public virtual DbSet<Turma> Turmas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=NOTE11-S19; initial catalog=dev_db_tarde; User Id =sa; Pwd= Senai@134;  TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Atividade>(entity =>
+        {
+            entity.HasKey(e => e.AtividadeId).HasName("PK__Atividad__742A5DF4F55ED59D");
+
+            entity.ToTable("Atividade");
+
+            entity.Property(e => e.Descricao)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Turma).WithMany(p => p.Atividades)
+                .HasForeignKey(d => d.TurmaId)
+                .HasConstraintName("FK__Atividade__Turma__4F7CD00D");
+        });
+
+        modelBuilder.Entity<Professor>(entity =>
+        {
+            entity.HasKey(e => e.ProfessorId).HasName("PK__Professo__90035949F573B171");
+
+            entity.ToTable("Professor");
+
+            entity.HasIndex(e => e.Email, "UQ__Professo__A9D105349D888E3B").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Nome)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Senha)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Turma>(entity =>
+        {
+            entity.HasKey(e => e.TurmaId).HasName("PK__Turma__BABB93048314FFC7");
+
+            entity.ToTable("Turma");
+
+            entity.Property(e => e.Nome)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Professor).WithMany(p => p.Turmas)
+                .HasForeignKey(d => d.ProfessorId)
+                .HasConstraintName("FK__Turma__Professor__4CA06362");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
