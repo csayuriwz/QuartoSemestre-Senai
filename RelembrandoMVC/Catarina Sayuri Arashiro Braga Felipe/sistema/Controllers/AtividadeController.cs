0cd7cfe17f1562afdaf6950ae03845ad1bd69fa9
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2024-10-17 17:19:10 -0300

    finalizacao MVC

---
+++ RelembrandoMVC/Catarina Sayuri Arashiro Braga Felipe/sistema/Controllers/AtividadeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using sistema.Contexts;
+using sistema.Models;
+
+namespace sistema.Controllers
+{
+    public class AtividadeController : Controller
+    {
+        private readonly SistemaContext _context;
+
+        public AtividadeController(SistemaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int turmaId)
+        {
+            var atividades = _context.Atividades
+                .Include(a => a.Turma)
+                .Where(a => a.TurmaId == turmaId);
+
+            var turma = _context.Turmas.FirstOrDefault(x => x.TurmaId == turmaId);
+
+            ViewBag.TurmaId = turmaId;
+            ViewBag.NomeTurma = turma!.Nome;
+
+            ViewBag.NomeProfessor = HttpContext.Session.GetString("Nome");
+
+            return View(atividades);
+
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarAtividade(int turmaId, string descricao)
+        {
+            var turma = _context.Turmas.FirstOrDefault(t => t.TurmaId == turmaId);
+
+            if (turma == null)
+            {
+                return View();
+            }
+
+            var novaAtividade = new Atividade
+            {
+                Descricao = descricao,
+                TurmaId = turmaId
+            };
+
+            _context.Atividades.Add(novaAtividade);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Atividades", new {turmaId});
+        }
+
+    }
+}
